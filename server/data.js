2ef4ada9a0d8af6069eabe3ced060b03378ae788
Pratek7 <pratikjungkarki123@gmail.com>
2023-01-11 18:32:48 +0545

    ok

--- server/data.js
+++ server/data.js
@@ -1,15 +1,15 @@
 const Data = [
   {
-    url: "https://www.tiktok.com/@pratek9090/video/7116491677132426523?is_from_webapp=1&sender_device=pc&web_id=7182504567610885633",
-    channel: "Pratek9",
+    url: "https://v16m.tiktokcdn.com/1f124d19ce38eadd2032908cc63ce6fa/63beec0f/video/tos/alisg/tos-alisg-pve-0037/oov7AfAKozxu1gBGhwJ7A3BPINnVBgAhd6UC8Q/?a=1180&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=4810&bt=2405&cs=0&ds=3&ft=pfusdMkF8Zmo0y~wW64jV9.-PpWrKsdm&mime_type=video_mp4&qs=0&rc=Z2ZmNWY0Zzo4NWVkOzY1ZkBpajozeDM6ZmRyaDMzODgzNEA1MWI2NDA0X2ExYzMuNi41YSNxc3AtcjRnM2tgLS1kLy1zcw%3D%3D&l=202301111103438F18D9A6E7719020E710&btag=80000&cc=3",
+    channel: "Random",
     songs: "NO Music supported",
     likes: "10000000",
     message: "50000",
-    des: "this is it guys",
+    desc: "this is it guys",
     shares: "10000",
   },
   {
-    url: "https://www.tiktok.com/@dipenchy6/video/7151346994768792858?is_from_webapp=1&sender_device=pc&web_id=7182504567610885633",
+    url: "https://v16-webapp-prime.tiktok.com/video/tos/maliva/tos-maliva-ve-0068c800-us/cda5725f0b1a48ccafaedb2a1404fb88/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C0%7C0&br=1550&bt=775&cs=0&ds=3&ft=4fUEKMkF8Zmo0KlwW64jV2ZkjpWrKsdm&mime_type=video_mp4&qs=0&rc=ZTc6Zjg8ODQ1NjU5OjU3OUBpM3N0cW90cjQ6djMzZjczM0A0Ly0uYTNeXy4xNWEtX2IyYSNeaXMzb3MvNjBfLS1eMTZzcw%3D%3D&btag=80000&expire=1673456422&l=20230111110009790AF5E575C5AA200150&ply_type=2&policy=2&signature=23d839cba1352020ebd58aa9aea12756&tk=tt_chain_token",
     channel: "Dipen vai",
     songs: "original sound - Dipen",
     likes: "3400",
@@ -27,7 +27,7 @@ const Data = [
     shares: "78",
   },
   {
-    url: "https://www.tiktok.com/@smokie_90/video/7159502719403740442?is_from_webapp=1&sender_device=pc",
+    url: "https://v16m.tiktokcdn.com/b416b8bb646502c55baa51707f8d1e59/63beebfc/video/tos/useast2a/tos-useast2a-pve-0037-aiso/owageNeAAjHEvmeHrHsEG1u6THRRhA4eW6IX2P/?a=1180&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=2874&bt=1437&cs=0&ds=3&ft=pfusdMkF8Zmo0y~wW64jV9.-PpWrKsdm&mime_type=video_mp4&qs=0&rc=M2Y5NWZpaDY6aTdlPDQ4O0Bpam1oOjc6ZnJxaDMzZjgzM0BjXmA2NF9jNTUxLjM0Y14uYSNhbnFucjQwZWtgLS1kL2Nzcw%3D%3D&l=202301111103438F18D9A6E7719020E710&btag=80000&cc=3",
     channel: "smokie_90",
     songs: "original sound - 8848",
     likes: "1000",
@@ -36,7 +36,7 @@ const Data = [
     shares: "56",
   },
   {
-    url: "https://www.tiktok.com/@footballvnp/video/7184366817375178010?is_from_webapp=1&sender_device=pc",
+    url: "https://v16m.tiktokcdn.com/12388155f81e2fe8fd64ff7d328cca6d/63beea02/video/tos/useast2a/tos-useast2a-pve-0037-aiso/ogW1rTo2NAIPqoG4QzKx4GAQdhhnfBNHJrCUWD/?a=1180&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=3020&bt=1510&cs=0&ds=3&ft=pfusdMkF8Zmo0LrwW64jVGN6PpWrKsdm&mime_type=video_mp4&qs=0&rc=Nzc4NDtoNDtpZ2c8ZjxoM0BpM21yeDY6Zm9xaDMzZjgzM0BeMC1eYTVhNjQxMDU2MWE2YSM1Yi8xcjRnNW5gLS1kL2Nzcw%3D%3D&l=202301111055171FCA8C95491B3B205509&btag=80000&cc=3",
     channel: "Football",
     songs: "Unstoppable(i put my armor)",
     likes: "7189",
@@ -45,7 +45,7 @@ const Data = [
     shares: "67",
   },
   {
-    url: "https://www.tiktok.com/@budhasubbamovies/video/7184732524025793794?is_from_webapp=1&sender_device=pc",
+    url: "https://v16m.tiktokcdn.com/4a7dc05d4f7ad2976af3f9aaa2148104/63beec64/video/tos/useast2a/tos-useast2a-pve-0037-aiso/oUTUQVlIypa2DujXUfezC1hMXzPRtsAcVQBxaP/?a=1180&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=3368&bt=1684&cs=0&ds=3&ft=pfusdMkF8Zmo0p8wW64jV_Z6PpWrKsdm&mime_type=video_mp4&qs=0&rc=aTY3NmU5Nzs6aDdoaTtpZEBpanY4OGk6ZnN3aDMzZjgzM0AzLTAyLmNeNl8xYjYzLjMtYSNfMzJtcjRvZV9gLS1kL2Nzcw%3D%3D&l=2023011111043956722C627F3DCC2139BF&btag=80000&cc=3",
     channel: "budhasubbamovies",
     songs: "original sound - BUDHA SUBBA MOVIES",
     likes: "39000",
@@ -54,7 +54,7 @@ const Data = [
     shares: "50",
   },
   {
-    url: "https://www.tiktok.com/@xy13344/video/7185774440490306842?is_from_webapp=1&sender_device=pc",
+    url: "https://v16m.tiktokcdn.com/4239a437ac024c6a3ea5eb9a923805be/63bee9fc/video/tos/useast2a/tos-useast2a-pve-0037c001-aiso/oQtCiQnzxAoIZD8fhBG9vAIXBhYvN1Zo5KUUGc/?a=1180&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=2638&bt=1319&cs=0&ds=3&ft=pfusdMkF8Zmo0LrwW64jVGN6PpWrKsdm&mime_type=video_mp4&qs=0&rc=OjVkZjs2aWhnZDpoPDs4ZkBpanM2eDs6Zmh0aDMzZjgzM0AxLWNfLmAuNWExNWBeYC8vYSNqbF9fcjQwL2tgLS1kL2Nzcw%3D%3D&l=202301111055171FCA8C95491B3B205509&btag=80000&cc=3",
     channel: "xy2234ok",
     songs: "orignal sound - xy",
     likes: "13000",
@@ -63,7 +63,7 @@ const Data = [
     shares: "34",
   },
   {
-    url: "https://www.tiktok.com/@pretiee7/video/7180772677114760474?is_from_webapp=1&sender_device=pc",
+    url: "https://v16-webapp.tiktok.com/99d078ac6a7b6e5fa074967c9d7b806a/63beeaf5/video/tos/useast2a/tos-useast2a-pve-0037c001-aiso/oAhIx6UnKaoUN1zBAotwfMAnQ1IRsBQWh50xOC/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=1614&bt=807&cs=0&ds=3&ft=4b~OyMkF8Zmo0dlwW64jV-yUPpWrKsdm&mime_type=video_mp4&qs=0&rc=N2g8aDllaDw6NDozZjZpOkBpM2s3OGQ6ZmVtaDMzZjczM0A0NC01Li1fNjUxM2M1YzYvYSNsLzMzcjRfaWdgLS1kMWNzcw%3D%3D&l=20230111105920444BA49B8B68AA2053A0&btag=80000",
     channel: "pretiee7♒♒",
     songs: "original sound - ♒♒",
     likes: "5000",
